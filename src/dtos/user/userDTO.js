2984ec1794d37f0ab3ea89354e5bd31e79387455
aguspeiretti <agupeiretti@gmail.com>
2023-07-19 23:51:43 -0300

    moks y errors listos

--- src/dtos/user/userDTO.js
+++ src/dtos/user/userDTO.js
@@ -1,6 +1,6 @@
 export default class UserDTO {
   constructor(user) {
-    (this.name = `${user.firstName} ${user.lastName}`),
+    (this.name = user.first_name),
       (this.role = user.role),
       (this.id = user._id),
       (this.email = user.email),
